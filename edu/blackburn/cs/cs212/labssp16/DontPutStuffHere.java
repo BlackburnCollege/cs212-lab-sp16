cd9b693eaaca3e8b67c540c716e9bb0710e9f570
joshua.gross <joshua.gross@HU130G04.Blackburn.College>
2016-01-21 13:46:49 -0600

    made a stupid don't save stuff here class

---
+++ edu/blackburn/cs/cs212/labssp16/DontPutStuffHere.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.blackburn.cs.cs212.labssp16;
+
+/**
+ *
+ * @author joshua.gross
+ */
+public class DontPutStuffHere {
+    public static void main(String[] args) {
+        System.out.println("Don't put stuff here!");
+        System.out.println("Create a sub-package");
+    }
+}
