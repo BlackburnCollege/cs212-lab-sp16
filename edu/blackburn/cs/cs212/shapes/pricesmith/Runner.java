a0862e1a8cf518bfe9fd35716bbbf327bcc052bb
olivia.price <olivia.price@HU130H003.Blackburn.College>
2016-01-28 11:01:29 -0600

    added classes

---
+++ edu/blackburn/cs/cs212/shapes/pricesmith/Runner.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.blackburn.cs.cs212.shapes.pricesmith;
+
+/**
+ *
+ * @author olivia.price
+ */
+public class Runner {
+    public void main(String args[]){
+        Measurement measureForR = new Measurement(5,6);
+        Rectangle rectangle = new Rectangle(5,6);
+        Measurement measureForS = new Measurement(5,5);
+        Square square = new Square(5,5);
+        
+    }
+    
+}
