ab216ff860052272cbc1b7cc15febfcbce7bf29a
shelby.rainford <shelby.rainford@HU130H007.Blackburn.College>
2016-02-18 10:51:06 -0600

    Finished Easy Mode, started Hard

--- edu/blackburn/cs/cs212sp16/fileio/rainfordholcomb/Runner.java
+++ edu/blackburn/cs/cs212sp16/fileio/rainfordholcomb/Runner.java
@@ -5,18 +5,24 @@
  */
 package edu.blackburn.cs.cs212sp16.fileio.rainfordholcomb;
 
+import java.io.*;
+
 /**
  *
  * @author ashley.holcomb
  */
 public class Runner {
+
     public static void main(String[] args) {
         testDivideByZero();
+        openFile("cs212-lab-sp16/edu/blackburn/cs/cs212sp16/fileio/rainfordholcomb/lab06.txt");
+        openFile("edu/blackburn/cs/cs212sp16/fileio/rainfordholcomb/lab06.txt");
     }
-    public static void testDivideByZero(){
+
+    public static void testDivideByZero() {
         System.out.println("Divide by zero Checkpoint 1");
         try {
-            int test = 7/0;
+            int test = 7 / 0;
             System.out.println("test = " + test);
             System.out.println("Divide by zero Checkpoint 2");
         } catch (Exception e) {
@@ -25,5 +31,38 @@ public class Runner {
         System.out.println("Divide by zero Checkpoint 3");
         System.out.println();
     }
+
+    public static void openFile(String filename) {
+        System.out.println("Opening File: " + filename);
+        System.out.println("Open/Read Checkpoint 1");
+        
+        try {
+            FileReader fr = new FileReader(filename);
+            BufferedReader br = new BufferedReader(fr);
+            System.out.println("Open/Read Checkpoint 2");
+            br.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        System.out.println("Open/Read Checkpoint 4");
+        System.out.println();
+        
+    }
     
+    public static void readFile(String filename){
+        System.out.println("Opening File: " + filename);
+        System.out.println("Open/Read Checkpoint 1");
+        
+        try {
+            FileReader fr = new FileReader(filename);
+            BufferedReader br = new BufferedReader(fr);
+            System.out.println("Open/Read Checkpoint 2");
+            //br.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        System.out.println("Open/Read Checkpoint 4");
+        System.out.println();
+    }
+
 }
