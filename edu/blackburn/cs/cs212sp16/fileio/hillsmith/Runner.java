28b3568a6ebb1a3992f9468654f749562a861a3f
Hunter.Hill <Hunter.Hill@HU130H008.Blackburn.College>
2016-02-18 10:51:22 -0600

    made dividebyzero and openFile

--- edu/blackburn/cs/cs212sp16/fileio/hillsmith/Runner.java
+++ edu/blackburn/cs/cs212sp16/fileio/hillsmith/Runner.java
@@ -5,6 +5,9 @@
  */
 package edu.blackburn.cs.cs212sp16.fileio.hillsmith;
 
+import java.io.*;
+
+
 /**
  *
  * @author Hunter.Hill
@@ -13,11 +16,14 @@ public class Runner {
 
     public static void main(String[] args) {
         testDivideByZero();
+        openFile("cs212-lab-sp16/edu/blackburn/cs/cs212sp16/fileio/hillsmith/lab06.txt");
+        openFile("abadfilename.mp3");
     }
+
     public static void testDivideByZero() {
         System.out.println("Divie by Zero checkpoint 1");
         try {
-            int i = 100/0;
+            int i = 100 / 0;
             System.out.println("Divide by Zero checkpoint 2");
         } catch (Exception e) {
             System.out.println("This doesn't work");
@@ -25,5 +31,20 @@ public class Runner {
         }
         System.out.println("Divide by zero checkpoint 3");
         System.out.println("");
+    }
+
+    public static void openFile(String filename) {
+        System.out.println("Opening file:" + filename);
+        System.out.println("Open/read checkpoint 1");
+        try {
+            FileReader fr = new FileReader(filename);
+            BufferedReader br = new BufferedReader(fr);
+            System.out.println("Open/read checkpoint 2");
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex);
+        }
+        System.out.println("Open/read checkpoint 4");
+        System.out.println("");
+       
     }
 }
