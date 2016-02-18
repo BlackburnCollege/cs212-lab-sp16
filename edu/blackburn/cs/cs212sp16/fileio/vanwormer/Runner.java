c6d237d9df99208405a4db1848cdd71fc17e25d3
admin.dakota <admin.dakota@HU130H010.Blackburn.College>
2016-02-18 14:53:52 -0600

    Did lab

--- edu/blackburn/cs/cs212sp16/fileio/vanwormer/Runner.java
+++ edu/blackburn/cs/cs212sp16/fileio/vanwormer/Runner.java
@@ -9,6 +9,7 @@ package edu.blackburn.cs.cs212sp16.fileio.vanwormer;
  *
  * @author admin.dakota
  */
+import java.io.*;
 public class Runner {
     
     public static void testDivideByZero() {
@@ -25,8 +26,52 @@ public class Runner {
         // stuff that happens after the exception throwing code
         System.out.println("Divide By Zerio Checkpoint 3");
         System.out.println();
-    } 
+    }
+    
+    public static void openFile(String filename) {
+        System.out.println("Opening File: " + filename);
+        System.out.println("Open Checkpoint 1");
+        try {
+        // Exception throwing code
+            FileReader fileReader = new FileReader(filename);
+            BufferedReader buffRead = new BufferedReader(fileReader);
+            System.out.println("Open Checkpoint 2");
+        } catch (Exception e) {
+        // what to do if and only if an exception is thrown
+        // for now, we'll just be printing out the exception
+        System.out.println("Exception found");
+        }
+        // stuff that happens after the exception throwing code
+        System.out.println("Open Checkpoint 3");
+        System.out.println();
+    }
+    
+    public static void readFile(String filename) {
+        String tmp = "Here's your file";
+        System.out.println("Opening File: " + filename);
+        System.out.println("Read Checkpoint 1");
+        try {
+        // Exception throwing code
+            FileReader fileReader = new FileReader(filename);
+            BufferedReader buffRead = new BufferedReader(fileReader);
+            do {
+                System.out.println(tmp);
+                tmp = buffRead.readLine();
+            } while(tmp != null);
+            System.out.println("Read Checkpoint 2");
+        } catch (IOException ex) {
+        // what to do if and only if an exception is thrown
+        // for now, we'll just be printing out the exception
+        System.out.println("No such file");
+        }
+        // stuff that happens after the exception throwing code
+        System.out.println("Read Checkpoint 3");
+        System.out.println();
+    }
+    
     public static void main(String[] args) {
         testDivideByZero();
+        openFile("lab06.txt");
+        readFile("lab06.txt");
     }
 }
