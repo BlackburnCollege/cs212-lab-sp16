a51c04f48818fd61bf412705895b9927dc4962c5
braydon.rekart <braydon.rekart@HU130H010.Blackburn.College>
2016-02-18 14:04:59 -0600

    made progress
    MY COMPUTER ALMOST GOT TURNED OFF :(

--- edu/blackburn/cs/cs212sp16/fileio/rekartlastname/Runner.java
+++ edu/blackburn/cs/cs212sp16/fileio/rekartlastname/Runner.java
@@ -5,6 +5,8 @@
  */
 package edu.blackburn.cs.cs212sp16.fileio.rekartlastname;
 
+import java.io.*;
+
 /**
  *
  * @author braydon.rekart
@@ -17,12 +19,25 @@ public class Runner {
 
     public static void testDivideByZero() {
         System.out.println("Divide By Zero Checkpoint 1");
-        try { 
+        try {
             int breakTheUniverse = 54563 / 0;
             System.out.println("Divide By Zero Checkpoint 2");
         } catch (Exception e) {
             System.out.println("Divide By Zero Checkpoint 2");
         }
         System.out.println();
+    }
+
+    public static void openFile(String filename) {
+
+        System.out.println("Opening File: " + filename);
+        System.out.println("Open/Read Checkpoint 1");
+        try {
+            FileReader fileReader = new FileReader
+        ("blackburn-cs212-sp16/edu/blackburn/cs/cs212-lab-sp16/lab06.txt");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
