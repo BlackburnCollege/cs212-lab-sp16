e28ba11451a10de71cf295875d708392b910a723
olivia.price <olivia.price@HU130H003.Blackburn.College>
2016-02-18 10:49:05 -0600

    finished easy mode

--- edu/blackburn/cs/cs212sp16/fileio/pricecramer/Runner.java
+++ edu/blackburn/cs/cs212sp16/fileio/pricecramer/Runner.java
@@ -4,14 +4,17 @@
  * and open the template in the editor.
  */
 package edu.blackburn.cs.cs212sp16.fileio.pricecramer;
-
+import java.io.*;
 /**
  *
  * @author olivia.price
  */
 public class Runner {
     public static void main(String[] args) {
-       
+        //not working filename
+       openFile("lab06.txt");
+       //working filename
+       openFile("cs212-lab-sp16/edu/blackburn/cs/cs212sp16/fileio/pricecramer/lab06.txt");
         DivideByZero();
     }
     public static void DivideByZero(){
@@ -25,5 +28,19 @@ public class Runner {
         }
          System.out.println("Divide By Zero Checkpoint 3");
          System.out.println("");
+    }
+    public static void openFile(String filename){
+        System.out.println("Opening File: " + filename);
+        System.out.println("Open/Read Checkpoint 1");
+        try{
+            FileReader fr = new FileReader(filename);
+            BufferedReader br = new BufferedReader(fr);
+            System.out.println("Open/Read Checkpoint 2");
+            br.close();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        System.out.println("Open/Read Checkpoint 4");
+        System.out.println("");
     }
 }
