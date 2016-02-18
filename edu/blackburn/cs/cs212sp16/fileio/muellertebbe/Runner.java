3b59618b391bec62b683a20264a565c24451461e
Jordan.Mueller <Jordan.Mueller@HU130H009.Blackburn.College>
2016-02-18 14:57:06 -0600

    finished medium mode

--- edu/blackburn/cs/cs212sp16/fileio/muellertebbe/Runner.java
+++ edu/blackburn/cs/cs212sp16/fileio/muellertebbe/Runner.java
@@ -5,6 +5,8 @@
  */
 package edu.blackburn.cs.cs212sp16.fileio.muellertebbe;
 
+import java.io.*;
+
 /**
  *
  * @author Jordan.Mueller
@@ -15,7 +17,9 @@ public class Runner {
 
     public static void main(String[] args) {
         testDivideByZero();
-        
+        String filename = "cs212-lab-sp16/edu/blackburn/cs/cs212sp16/fileio/muellertebbe/lab06.txt";
+        openFile(filename);
+        openFile("lab06.txt");
     }
 
     public static int testDivideByZero() {
@@ -35,6 +39,19 @@ public class Runner {
         System.out.println("Divide By Zero Checkpoint 3");
         System.out.println("");
         return var;
+    }
+    public static void openFile(String filename){
+        System.out.println("Opening File: " + filename);
+        System.out.println("Open Checkpoint 1");
+        try { 
+            FileReader fr = new FileReader(filename);
+            BufferedReader br = new BufferedReader(fr);
+            System.out.println("Open Checkpoint 2");
+            } catch (Exception e) {
+                System.out.println(e);
+        }
+        System.out.println("Open Checkpoint 3");
+        System.out.println("");
     }
 
 }
