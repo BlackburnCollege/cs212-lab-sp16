c831eba0f1fc6d8777424078bf31764e0785738e
braydon.rekart <braydon.rekart@HU130H010.Blackburn.College>
2016-03-17 14:11:16 -0500

    added linkedlist classes

---
+++ edu/blackburn/cs/cs212sp16/linkedlist/rekartlastname/LinkedList.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.blackburn.cs.cs212sp16.linkedlist.rekartlastname;
+
+/**
+ *
+ * @author braydon.rekart
+ */
+public class LinkedList {
+    private ListElement firstElement;
+    private ListElement currentElement;
+    
+    public LinkedList() {
+        
+    }
+    public void add(Account a){
+        
+    }
+}
---
+++ edu/blackburn/cs/cs212sp16/linkedlist/rekartlastname/Runner.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.blackburn.cs.cs212sp16.linkedlist.rekartlastname;
+
+/**
+ *
+ * @author braydon.rekart
+ */
+public class Runner {
+    public static void main(String[] args) {
+        
+    }
+}
