fb54ffd91d25ed55ffe07b4b0b5a5c76fe560609
dakota.tebbe <dakota.tebbe@HU130H002.Blackburn.College>
2016-03-24 15:04:51 -0500

    Lab stuff

---
+++ edu/blackburn/cs/cs212sp16/triage/tebbevanwormer/TriageQueue.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.blackburn.cs.cs212sp16.triage.tebbevanwormer;
+import edu.blackburn.cs.cs212sp16.er.*;
+/**
+ *
+ * @author dakota.tebbe
+ */
+public class TriageQueue extends AbstractQueue {
+    
+}
